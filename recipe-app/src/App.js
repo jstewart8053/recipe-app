cdcfb0ee7cf5ec36cbb6698f2b7a69360365c918
Jennifer Stewart <jstewart8053@gmail.com>
2020-10-20 18:41:06 -0500

    added api key

---
+++ recipe-app/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+const App = () => {
+
+  const APP_ID = '3a5f4ed1';
+  const APP_KEY = 'fd17e1884cb7290bb0514c046e39c6ce'
+
+
+
+  return (
+    <div className="App">
+      <h1>Hello React </h1>
+    </div>
+  );
+};
+
+export default App;
